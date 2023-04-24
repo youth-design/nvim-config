e22c6261484b840981e3b5a802c0715b532a8df5
alexey <youth-design@yandex.ru>
2023-04-24 21:01:56 +0300

    Add Telescope git keybindings

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -5,3 +5,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fz', "<cmd> Telescope current_buffer_fuzzy_find <CR>", {})
+
+-- git
+vim.keymap.set('n', '<leader>cm', "<cmd> Telescope git_commits <CR>", {})
+vim.keymap.set('n', '<leader>gs', "<cmd> Telescope git_status <CR>", {})
